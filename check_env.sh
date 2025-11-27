1d1f9d201ea9429fef833f52965ee86cdf66dc27
Mike Monaco <mamonaco1973@gmail.com>
2025-11-27 10:11:09 -0500

    v1 of azure xubuntu

--- check_env.sh
+++ check_env.sh
@@ -3,7 +3,7 @@
 echo "NOTE: Validating that required commands are found in your PATH."
 
 # List of required commands
-commands=("az" "terraform" "jq")
+commands=("az" "terraform" "jq" "packer")
 
 # Flag to track if all commands are found
 all_found=true
